4a4b6b9a0945cecd3103fb45367c306a0ff2c571
mikeberezowski <mike.berezowski@gmail.com>
2019-06-17 15:56:31 -0700

    Adding enums for card suits and documentation

---
+++ PokerApp/PokerConsoleApp/Enums/CardSuit.cs
@@ -0,0 +1,10 @@
+﻿namespace PokerConsoleApp.Enums
+{
+    public enum CardSuit
+    {
+        Spades = 0,
+        Diamonds = 1,
+        Clubs = 2,
+        Hearts = 3
+    }
+}
---
+++ PokerApp/PokerConsoleApp/Enums/PokerHandRank.cs
@@ -0,0 +1,10 @@
+﻿namespace PokerConsoleApp.Enums
+{
+    public enum PokerHandRank
+    {
+        HighCard = 0,
+        OnePair = 1,
+        ThreeOfAKind = 2,
+        Flush = 3
+    }
+}
